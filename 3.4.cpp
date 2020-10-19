1b29955f7a28fe488133bbc1b528f8651d8c84a7
FloopyG <36450035+FloopyG@users.noreply.github.com>
2020-10-19 12:12:53 +0300

    Initial commit

---
+++ 3.4.cpp
@@ -0,0 +1,31 @@
+﻿// Lab3_4.cpp
+// Огонюк Назар
+// Лабораторна робота No 3.4
+// Розгалуження, задане плоскою фігурою.
+// Варіант 12
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    double R;
+    double x;
+    double y;
+
+    cout << "R = "; cin >> R;
+    cout << "x = "; cin >> x;
+    cout << "y = "; cin >> y;
+
+    if ((y <= 0 && x >= 0 && x < R && y > -R && pow(x - R, 2) + pow(y + R, 2) > pow(R, 2)) ||
+        (y >= 0 && x <= 0 && x > -R && y < R && pow(x + R, 2) + pow(y - R, 2) > pow(R, 2)))
+        cout << "Yes" << endl;
+    else
+        cout << "No" << endl;
+
+    system("pause");
+    cin.get();
+    return 0;
+}
